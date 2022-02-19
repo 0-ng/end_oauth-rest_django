2c2f98fef44e5ba38781dd1c192548fafd46db84
0ng <1427508598@qq.com>
2022-02-20 00:35:26 +0800

    connect mysql

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,7 @@
 FROM python:3.6
 
-#RUN mkdir -p /usr/src/app
 RUN python -m pip install -U --force-reinstall pip
 
-#RUN git clone --recursive "https://github.com/0-ng/end_web.git" /usr/src/app
 WORKDIR /usr/src/app
 COPY ./ ./
 RUN pip install --no-cache-dir -r /usr/src/app/requirements.txt  -i https://pypi.tuna.tsinghua.edu.cn/simple
--- rest/settings.py
+++ rest/settings.py
@@ -86,10 +86,10 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': BASE_DIR / 'db.mysql',
-        'USER': '0ng',
+        'NAME': 'mysql',
+        'USER': 'root',
         'PASSWORD': '123qwe',
-        'HOST': '127.0.0.1',
+        'HOST': 'mysql',
         'PORT': '3306',
     }
 }
